70d48849e70520b21cbce474223b67bbd974965e
Pawan Kumar D <pawanseerwani@gmail.com>
2014-11-20 00:02:39 +0530

    add image access in testNet2 func

--- background.js
+++ background.js
@@ -4,18 +4,27 @@ console.log("inside bg.js");
 
 function success()
 {
+  show_alert = false;
   chrome.browserAction.setIcon({ path: 'online-icon.png'})
   chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
   chrome.browserAction.setBadgeText({text: 'Up'});
-  console.log("online");
+  console.log("online-image");
 }
+
+var show_alert = false;
 
 function failure()
 {
   chrome.browserAction.setIcon({ path: 'offline-icon.png'})
   chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
   chrome.browserAction.setBadgeText({text: 'Down'});
-  console.log("offline");    
+  console.log("offline-image");
+  if (show_alert == false)
+  {
+    show_alert = true;    
+    alert("offline"); 
+
+  }
 }
 
 function testNet(){
@@ -27,43 +36,10 @@ function testNet(){
 
 function testNet2()
 {
-  var xmlhttp;
-  xmlhttp=new XMLHttpRequest();
+  var i = new Image();
+  i.onload = success;
+  i.onerror = failure;
 
-  xmlhttp.onreadystatechange=function(){
-    if(xmlhttp.readyState==4 && xmlhttp.status==200){
-      success();
-    }
-    else
-    {
-      console.log(xmlhttp);
-    }
-  }
-  var url = "https://www.google.co.in/search?q=hello";
-  xmlhttp.open("GET", url, false);
-  xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-  xmlhttp.setRequestHeader("Data-Type", "jsonp");
-  xmlhttp.send();
+  i.src = 'http://gfx2.hotmail.com/mail/uxp/w4/m4/pr014/h/s7.png?d=' + escape(Date());
 }
 setInterval(testNet, 1000);
-
-
-
-
-  // TODO : Figure out a way to use jquery in background.js
-   // $.ajax({
-   //          url: "http://www.google.com",
-   //          context: document.body,
-   //          error: function(jqXHR, exception) {
-   //            chrome.browserAction.setIcon({ path: 'offline-icon.png'})
-   //            chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-   //            chrome.browserAction.setBadgeText({text: 'Down'});
-   //            console.log("offline");
-   //          },
-   //          success: function(){
-   //            chrome.browserAction.setIcon({ path: 'online-icon.png'})
-   //            chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
-   //            chrome.browserAction.setBadgeText({text: 'Up'});
-   //            console.log("online");
-   //          }
-   //        });
