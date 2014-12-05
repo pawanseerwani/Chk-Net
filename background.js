9d339b5c9e7d0e34a80196a8e474eeb67e764b9c
Pawan Kumar D <pawanseerwani@gmail.com>
2014-12-05 09:50:03 +0530

    remove popups on startup

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
+var first_time_success  = true;
+var first_time_failure  = true;
 var showed_offline_alert = false;
 var showed_online_alert = false;
 
 function success()
 {
+  if (first_time_success == true)
+  {
+    first_time_success = false;
+    showed_online_alert = true; // Need to falsely put true to avoid showing popup at startup
+    return;
+  }
   showed_offline_alert = false;
   chrome.browserAction.setIcon({ path: 'online-icon.png'})
   chrome.browserAction.setBadgeBackgroundColor({ color: [0, 255, 0, 255] });
@@ -17,6 +25,11 @@ function success()
 
 function failure()
 {
+  if (first_time_failure == true)
+  {
+    first_time_failure = false;
+    return;
+  }
   showed_online_alert = false;
   chrome.browserAction.setIcon({ path: 'offline-icon.png'})
   chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
